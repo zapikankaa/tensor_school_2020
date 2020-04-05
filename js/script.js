9632355d02754f8c0a4d26ecd4ddb016c68c266e
zapikankaa <tugaevad@gmail.com>
2020-04-05 22:54:06 +0500

    Something done

--- js/script.js
+++ js/script.js
@@ -1,11 +1,109 @@
-window.addEventListener('load', setEventListeners);
+let studentsData = [
+    {
+        "fullname": "Маша Иванова",
+        "birthday": "1.05.1999",
+        "university": "УГАТУ",
+        "course": 2,
+        "photo": "../img/arya.jpg",
+        "telephone": "79994561236"
+    },
+    {
+        "fullname": "Вавилен Татарский",
+        "birthday": "8.01.1997",
+        "university": "СурГУ",
+        "course": 3,
+        "photo": "../img/cold_king.jpg",
+        "telephone": "79965875056"
+    },
+    {
+        "fullname": "Даниэль Риверо Падилья",
+        "birthday": "18.12.1991",
+        "university": "БГУ",
+        "course": 4,
+        "photo": "../img/jon_snow.jpg",
+        "telephone": "79056655636"
+    },
+    {
+        "fullname": "Маргарита Анна-Лиза фон Гейзенберг",
+        "birthday": "23.10.1994",
+        "university": "УГАТУ",
+        "course": 2,
+        "photo": "../img/cersei.jpg",
+        "telephone": ""
+    },
+    {
+        "fullname": "Миша Петров",
+        "birthday": "11.05.1997",
+        "university": "УрФУ",
+        "course": 3,
+        "photo": "../img/ramsay.jpg"
+    },
+    {
+        "fullname": "Ян Коган",
+        "birthday": "19.07.1994",
+        "university": "БГУ",
+        "course": 4,
+        "photo": "../img/tyrion.jpg"
+    },
+    {
+        "fullname": "Симон Гроф",
+        "birthday": "15.11.1998",
+        "university": "УГАТУ",
+        "course": 2,
+        "photo": "../img/ygritte.jpg"
+    },
+    {
+        "fullname": "Марат Сидоров",
+        "birthday": "14.08.1999",
+        "university": "СурГУ",
+        "course": 3,
+        "photo": "../img/tormund.jpg"
+    }
+];
+
+class Student {
+    constructor(data) {
+        this.fullname = data.fullname;
+        this.birthday = data.birthday;
+        this.university = data.university;
+        this.course = data.course;
+        this.photo = data.photo;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function setEventListeners() {
     let students = document.getElementsByClassName('student');
     for (student of students) {
         student.addEventListener('click', showModal);
     }
-    let modalClose = document.getElementsByClassName('close')[0];
+    let modalClose = document.querySelector('.close');
     modalClose.addEventListener('click', hideModal);
 }
 
@@ -57,3 +155,5 @@ function hideModal() {
     let modal = document.getElementsByClassName('modal')[0];
     modal.style.display = 'none';
 }
+
+setEventListeners();
